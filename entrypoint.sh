b5019659dee285ff3963e2564b2aa48989df90a4
miigotu <miigotu@gmail.com>
2023-10-20 14:11:28 -0400

    output release information for use in other tools
    add capability to use generated information in other release tools for releasing to other endpoints such as pypi, docker, debchange, etc

--- entrypoint.sh
+++ entrypoint.sh
@@ -74,3 +74,9 @@ if [ "${CREATE_RELEASE}" = "true" ] || [ "${CREATE_RELEASE}" = true ]; then
 fi
 
 echo "release=${NEXT_RELEASE}" >>$GITHUB_OUTPUT
+echo "title=${NAME}" >>$GITHUB_OUTPUT
+echo "changelog=${MESSAGE}" >>$GITHUB_OUTPUT
+echo "draft=${DRAFT}" >>$GITHUB_OUTPUT
+echo "pre=${PRE}" >>$GITHUB_OUTPUT
+
+
